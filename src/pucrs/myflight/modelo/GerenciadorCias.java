148547ef2de98752e6c61ad48b86ff7188644824
lucassmann <lucas.assmann@edu.pucrs.br>
2021-11-08 19:44:05 -0300

    GerenciadorCias implementado de acordo com modelo. Metodos buscar retornam null se não acham.

--- src/pucrs/myflight/modelo/GerenciadorCias.java
+++ src/pucrs/myflight/modelo/GerenciadorCias.java
@@ -8,4 +8,33 @@ public class GerenciadorCias {
 	public GerenciadorCias() {
 		empresas = new ArrayList<>();
 	}
+
+	public void adicionar(CiaAerea cia){
+		empresas.add(cia);
+	}
+
+	public ArrayList<CiaAerea> listarTodas(){
+		return this.empresas;
+	} 
+
+	//Buscar funciona, mas dá NullPointerException se não encontra. Outra opção seria retornar uma CiaAerea vazia.
+	public CiaAerea buscarCodigo(String cod){
+		for (CiaAerea cia : empresas){
+			if (cia.getCodigo().equals(cod)){
+				return cia;
+			}
+		}
+		return null;
+	}
+	
+	//Buscar funciona, mas dá NullPointerException se não encontra. Outra opção seria retornar uma CiaAerea vazia.
+	//modelagem especifica argumento cod, mas faria mais sentido chamar de nome
+	public CiaAerea buscarNome(String cod){
+		for (CiaAerea cia : empresas){
+			if (cia.getNome().equals(cod)){
+				return cia;
+			}
+		}
+		return null;
+	} 	
 }
