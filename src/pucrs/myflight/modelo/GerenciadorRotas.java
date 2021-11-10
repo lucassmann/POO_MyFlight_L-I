ed4b13b8fc813d415abfa46e536b42f8405ad43a
lucassmann <lucas.assmann@edu.pucrs.br>
2021-11-10 00:55:14 -0300

    GerenciadorRotas implementado tal como no modelo da atividade 1. Com excessão de Comparable de Aeronaves, que já fiz uso.

--- src/pucrs/myflight/modelo/GerenciadorRotas.java
+++ src/pucrs/myflight/modelo/GerenciadorRotas.java
@@ -1,5 +1,29 @@
 package pucrs.myflight.modelo;
 
-public class GerenciadorRotas {
+import java.util.ArrayList;
 
+public class GerenciadorRotas {
+    private ArrayList<Rota> rotas;
+
+    public GerenciadorRotas(){
+        rotas = new ArrayList<>();
+    }
+
+    public void adicionar(Rota rota){
+        rotas.add(rota);
+    }
+
+    public ArrayList<Rota> listarTodas(){
+        return this.rotas;
+    }
+
+    public ArrayList<Rota> buscarPorOrigem(Aeroporto orig){
+        ArrayList<Rota> aux = new ArrayList<>();
+        for(Rota rota : rotas){
+            if(rota.getOrigem().equals(orig)){
+                aux.add(rota);
+            }
+        }
+        return aux;
+    }
 }
