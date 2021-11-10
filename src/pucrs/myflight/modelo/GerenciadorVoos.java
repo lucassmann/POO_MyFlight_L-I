33c12086ee45c944fd6c90642dc9027311cae6d1
lucassmann <lucas.assmann@edu.pucrs.br>
2021-11-10 00:34:49 -0300

    Classe GerenciadorVoos tal como no modelo do exercicio 1.

--- src/pucrs/myflight/modelo/GerenciadorVoos.java
+++ src/pucrs/myflight/modelo/GerenciadorVoos.java
@@ -1,5 +1,36 @@
 package pucrs.myflight.modelo;
 
-public class GerenciadorVoos {
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
+public class GerenciadorVoos {
+    private ArrayList<Voo> voos;
+
+    public GerenciadorVoos(){
+        voos = new ArrayList<>();
+    }
+
+    public void adicionar(Voo voo){
+        voos.add(voo);
+    }
+
+    public ArrayList<Voo> listarTodos(){
+        return this.voos;
+    }
+
+    public ArrayList<Voo> buscarData(LocalDate data){
+       ArrayList<Voo> aux = new ArrayList<>();
+       for(Voo voo : voos){
+           if(voo.getDatahora().toLocalDate().equals(data)){
+               aux.add(voo);
+           }
+       }
+       return aux;
+        
+    }
+
+    
 }
