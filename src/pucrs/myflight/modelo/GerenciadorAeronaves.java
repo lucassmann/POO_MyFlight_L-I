210fbc02d0fdbced71641be872e997ec8c3e02c0
lucassmann <lucas.assmann@edu.pucrs.br>
2021-11-10 01:14:08 -0300

    GerenciadorAeronaves assim como modelo na atividade1.

--- src/pucrs/myflight/modelo/GerenciadorAeronaves.java
+++ src/pucrs/myflight/modelo/GerenciadorAeronaves.java
@@ -1,5 +1,28 @@
 package pucrs.myflight.modelo;
 
-public class GerenciadorAeronaves {
+import java.util.ArrayList;
 
+public class GerenciadorAeronaves {
+    private ArrayList<Aeronave> aeronaves;
+
+    public GerenciadorAeronaves(){
+        aeronaves = new ArrayList<>();   
+    }
+
+    public void adicionar(Aeronave aviao){
+        aeronaves.add(aviao);
+    }
+
+    public ArrayList<Aeronave> listarTodas(){
+        return this.aeronaves;
+    }
+
+    public Aeronave buscarPorCodigo(String cod){
+        for(Aeronave aviao : aeronaves){
+            if(aviao.getCodigo().equals(cod)){
+                return aviao;
+            }
+        }
+        return null;
+    }
 }
