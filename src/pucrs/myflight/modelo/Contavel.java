d4b89f26c19e79324ce9c0d518f6777205dc3f56
lucassmann <lucas.assmann@edu.pucrs.br>
2021-11-11 17:20:55 -0300

    Interface Contavel

---
+++ src/pucrs/myflight/modelo/Contavel.java
@@ -0,0 +1,9 @@
+package pucrs.myflight.modelo;
+
+interface Contavel {
+    //public int contador = 0;     //nao sei se a classe vai pegar isso se colocar
+
+    public static int Size(){
+        return 0;
+    };
+    }
