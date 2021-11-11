db71dec74d9a8849cf442dff36aa94cf56a5cd36
lucassmann <lucas.assmann@edu.pucrs.br>
2021-11-10 21:55:34 -0300

    distancia nao static

--- src/pucrs/myflight/consoleApp/App.java
+++ src/pucrs/myflight/consoleApp/App.java
@@ -1,5 +1,7 @@
 package pucrs.myflight.consoleApp;
 
+import pucrs.myflight.modelo.Geo;
+
 public class App {
 
 	public static void main(String[] args) {
--- src/pucrs/myflight/modelo/Geo.java
+++ src/pucrs/myflight/modelo/Geo.java
@@ -31,5 +31,15 @@ public class Geo {
 		return d;
 	}
 
-	
+	public double distancia(Geo g2) {
+		double lat1 = Math.toRadians(this.getLatitude());
+		double lat2 = Math.toRadians(g2.getLatitude());
+		double lon1 = Math.toRadians(this.getLongitude());
+		double lon2 = Math.toRadians(g2.getLongitude());
+		double sin1 = Math.sin((lat1 - lat2)/2) * Math.sin((lat1 - lat2)/2);
+		double sin2 = Math.sin((lon1 - lon2)/2) * Math.sin((lon1 - lon2)/2);
+		double raiz = Math.sqrt(sin1 + sin2 * Math.cos(lat1) * Math.cos(lat2));
+		double d = (2 * 6371) * Math.asin(raiz);
+		return d;
+	}
 }
