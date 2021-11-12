e9920efec5bec3f0d73350f0a17649ad919306af
lucassmann <lucas.assmann@edu.pucrs.br>
2021-11-12 00:52:56 -0300

    Teste geral FUNCIONA!!!! \o/

--- src/pucrs/myflight/consoleApp/App.java
+++ src/pucrs/myflight/consoleApp/App.java
@@ -1,9 +1,31 @@
 package pucrs.myflight.consoleApp;
 
+import java.io.IOException;
+import java.time.Duration;
+import java.time.LocalDateTime;
+
+import pucrs.myflight.modelo.CiaAerea;
+import pucrs.myflight.modelo.GerenciadorAeronaves;
+import pucrs.myflight.modelo.GerenciadorAeroportos;
+import pucrs.myflight.modelo.GerenciadorCias;
+import pucrs.myflight.modelo.GerenciadorRotas;
+import pucrs.myflight.modelo.Rota;
+import pucrs.myflight.modelo.VooDireto;
+
 public class App {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		System.out.println("\nMyFlight project...");
 
+		GerenciadorRotas gerRotas = new GerenciadorRotas();
+		GerenciadorAeronaves gerAeronaves = new GerenciadorAeronaves();
+        GerenciadorAeroportos gerAeroportos = new GerenciadorAeroportos();
+        GerenciadorCias gerCias = new GerenciadorCias();
+		
+		LocalDateTime datahora = LocalDateTime.of(2016, 8, 12, 12, 0, 0);
+		Duration duracao = Duration.ofHours(1);
+		VooDireto voodir = new VooDireto(gerRotas.listarTodas().get(1), datahora, duracao);
+		System.out.println(voodir.getDatahora()); //FUNCIONA!!!!!!!!  \o/
+		
 	}
 }
