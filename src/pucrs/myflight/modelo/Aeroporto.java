faca7a1e7856749b8f55451f8f07daac351597b9
lucassmann <lucas.assmann@edu.pucrs.br>
2021-11-12 16:43:56 -0300

    Documentação de Aeroporto

--- src/pucrs/myflight/modelo/Aeroporto.java
+++ src/pucrs/myflight/modelo/Aeroporto.java
@@ -5,6 +5,13 @@ public class Aeroporto implements Comparable<Aeroporto>{
 	private String nome;
 	private Geo loc;
 	
+	/**
+	 * 
+	 * @param codigo código do aeroporto
+	 * @param nome nome do aeroporto
+	 * @param loc objeto Geo que representa a localização do aeroporto. Um objeto Geo possui 2 variáveis double para representar latitude e longitude.
+	 * Aeroporto implementa a interface Comparable<Aeroporto>
+	 */
 	public Aeroporto(String codigo, String nome, Geo loc) {
 		this.codigo = codigo;
 		this.nome = nome;
@@ -22,7 +29,10 @@ public class Aeroporto implements Comparable<Aeroporto>{
 	public Geo getLocal() {
 		return loc;
 	}
-
+	
+	/**
+	 * O método compareTo da interface Comparable é implementado de forma a comparar a String nome de um objeto Aeroporto.
+	 */
 	@Override
 	public int compareTo(Aeroporto o) {
 		return this.getNome().compareTo(o.getNome());
