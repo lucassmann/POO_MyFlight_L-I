2cb264d0f3c7271a04de2680befc7bf841e1ee7c
lucassmann <lucas.assmann@edu.pucrs.br>
2021-11-12 16:43:37 -0300

    Documentação de Aeronave

--- src/pucrs/myflight/modelo/Aeronave.java
+++ src/pucrs/myflight/modelo/Aeronave.java
@@ -5,6 +5,13 @@ public class Aeronave implements Contavel{
 	private String descricao;
 	private static int count=0;
 	
+	/**
+	 * 
+	 * @param codigo código da aeronave.
+	 * @param descricao descrição da aeronave. 
+	 * 
+	 * Os dados utilizados para popular os objetos estão armazenados nos arquivos .dat da pasta data.
+	 */
 	public Aeronave(String codigo, String descricao) {
 		this.codigo = codigo;
 		this.descricao = descricao;
